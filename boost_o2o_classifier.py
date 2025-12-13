f4c3de19c5bb3d41b16aa599419d5f019dba15f8
jadmax0w0 <jadmax@qq.com>
2025-12-13 13:36:59 +0800

    修正了模型命名错误，将 boost 改为 bag，并更新相关函数调用

--- boost_o2o_classifier.py
+++ boost_o2o_classifier.py
@@ -21,13 +21,13 @@ from xgboost import XGBClassifier
 def build_bagger_models(ref_y_train: NDArray, with_id: bool = False):
     ratio = float(sum(ref_y_train == 0)) / sum(ref_y_train == 1)
 
-    boost_models = [
+    bagged_models = [
         SVC(kernel='rbf', C=5, gamma='scale', class_weight='balanced'),
         XGBClassifier(
             n_estimators=200,
-            max_depth=6,              # XGBoost 不喜深树，一般 3-10 之间
+            max_depth=6,
             learning_rate=0.1,
-            scale_pos_weight=ratio,   # 【最关键】专门应对样本不平衡
+            scale_pos_weight=ratio,
             use_label_encoder=False,
             eval_metric='logloss',
             n_jobs=-1,
@@ -40,20 +40,20 @@ def build_bagger_models(ref_y_train: NDArray, with_id: bool = False):
             n_estimators=200,
             learning_rate=0.1,
             max_depth=10,
-            max_features='sqrt',       # 核心：处理高维文本特征
-            min_samples_leaf=10,       # 核心：忽略生僻词造成的过细分裂
+            max_features='sqrt',
+            min_samples_leaf=10,
             subsample=0.8,
             random_state=42
         ),
     ]
 
     if with_id:
-        boost_models = [(f"{i}", model) for i, model in enumerate(boost_models)]
+        bagged_models = [(f"{i}", model) for i, model in enumerate(bagged_models)]
 
-    return boost_models
+    return bagged_models
 
 
-def run_boost(x_train, x_test, y_train, y_test, model_path, save_path):
+def run_bagger(x_train, x_test, y_train, y_test, model_path, save_path):
     if model_path is None:
         models = build_bagger_models(y_train, with_id=False)
         bagger = VotingBagger(*models)
@@ -96,7 +96,7 @@ if __name__ == "__main__":
         x_train, x_val, y_train, y_val = otils.feat_extraction(df_train, vectorizer, val_split_rate=0.2)
         print(f"Load O2O data complete: training set - {x_train.shape}{(', val set - ' + str(x_val.shape)) if x_val is not None else ''}")
 
-        run_boost(x_train, x_val, y_train, y_val, model_path=args.model, save_path=args.model_output)
+        run_bagger(x_train, x_val, y_train, y_val, model_path=args.model, save_path=args.model_output)
 
     else:
         df_train = otils.load_o2o_csv("./train.csv")
@@ -106,7 +106,7 @@ if __name__ == "__main__":
         x_train, _, y_train, _ = otils.feat_extraction(df_train, vectorizer)
         x_test, _, _, _ = otils.feat_extraction(df_test, vectorizer)
 
-        y_pred = run_boost(x_train, x_test, y_train, None, args.model, args.model_output)
+        y_pred = run_bagger(x_train, x_test, y_train, None, args.model, args.model_output)
         print(f"Prediction complete, {y_pred.sum().item()} positive samples discovered")
 
         if args.test_break:
